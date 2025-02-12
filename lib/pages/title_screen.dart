70b7322020faca3fc2aef9a2a32e3ee3cdf379d5
enklht <enklht@gmail.com>
2025-02-12 19:11:53 +0900

    refactor: :recycle: create button with a function

--- lib/pages/title_screen.dart
+++ lib/pages/title_screen.dart
@@ -2,6 +2,38 @@ import "package:flutter/material.dart";
 import "package:street_performance_helper/pages/team_search_screen.dart";
 
 class TitleScreen extends StatelessWidget {
+  Widget _createLinkButton(
+    BuildContext context,
+    String data,
+    Widget destination,
+  ) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => destination,
+          ),
+        );
+      },
+      child: SizedBox(
+        width: 200,
+        height: 50,
+        child: Center(
+          child: Text(
+            data,
+            style: const TextStyle(fontSize: 20),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,16 +45,13 @@ class TitleScreen extends StatelessWidget {
         ),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => const TeamSearchScreen(),
-              ),
-            );
-          },
-          child: const Text("チーム検索"),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            _createLinkButton(context, "チーム検索", const TeamSearchScreen()),
+            const SizedBox(height: 20),
+            _createLinkButton(context, "技検索", const TeamSearchScreen()),
+          ],
         ),
       ),
     );
